b1651ffe3db5c7ebbafe2dbae57ffe6319df695b
Bartosz Grzys <b.grzys@gmail.com>
2017-08-02 15:59:02 +0200

    Welcome screen.

--- vr/components/StartGame.js
+++ vr/components/StartGame.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, View, VrButton } from 'react-vr';
+import { Text, View, VrButton, asset, Model} from 'react-vr';
 import styles from '../styles/main';
 import { startNewGame } from '../actions';
 
@@ -13,13 +13,34 @@ class StartGame extends Component {
   render() {
     return (
       <View style={styles.gameStyle}>
-        <Text style={styles.text}>Welcome to VRSarus</Text>
+        <Model
+          style={{
+            transform: [{ translate: [0, 1, -4] }, { scale: 0.03 }],
+            flex: 1,
+            width: 0.5,
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}
+          source={{
+            obj: asset('welcome/welcome.obj'),
+            mtl: asset('welcome/welcome.mtl'),
+          }}
+        />
 
-        <VrButton
-          style={styles.button}
-          onClick={() => this.props.startNewGame()}
-        >
-          <Text style={styles.buttonText}>Play Game!</Text>
+        <VrButton onClick={() => this.props.startNewGame()}>
+          <Model
+            style={{
+              transform: [{ translate: [0, 0, -3] }, { scale: 0.02 }],
+              flex: 1,
+              width: 0.5,
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}
+            source={{
+              obj: asset('start/start.obj'),
+              mtl: asset('start/start.mtl'),
+            }}
+          />
         </VrButton>
       </View>
     );
